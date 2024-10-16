c325933ef0d2dcb93688ea202c8eed0c09fc31fc
Manav Panwar <manav.panwar318@gmail.com>
2024-10-16 15:14:40 +0530

    public

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def ask():
     return jsonify({'response': 'Error fetching column details.'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
